5066f6069940c87c19ab93b4ebd2e45c66b2220a
tjdbssy137 <65931605+tjdbssy137@users.noreply.github.com>
2024-03-04 01:28:43 +0900

    assignment2

---
+++ homework/20240228_01/20240228_01/Character.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+// 실습2
+// gotoxy()사용해서 캐릭터 만들기
+
+void gotoxy(int x, int y)
+{
+	COORD pos = { x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+void setcolor(int color, int bgcolor)
+{
+	color &= 0xf;
+	bgcolor &= 0xf;
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (bgcolor << 4) | color);
+}
+
+void main()
+{
+	//system("pause"); // 일시정지
+	//system("cls"); // 화면 밀어버림
+	gotoxy(0, 10);
+	setcolor(10, 0); // 색상 변경
+	printf("□□□□□□□□□□□□□□□□\n");
+	printf("□□□□□□■■■■□□□□□□\n");
+	printf("□□□□□□■■■■□□□□□□\n");
+	printf("□□□□■■■■■■■■□□□□\n");
+	printf("□□□■■□■■■■□■■□□□\n");
+	printf("□□■■□■■■■■■□■■□□\n");
+	printf("□■■□□□■■■■□□□■■□\n");
+	printf("□□□□□■■■■■■□□□□□\n");
+	printf("□□□□■■□□□□■■□□□□\n");
+	printf("□□□□■■□□□□■■□□□□\n");
+	printf("□□□■■■□□□□■■■□□□\n");
+	printf("□□□□□□□□□□□□□□□□\n");
+
+	//콘솔 색상 바꾸기
+	//콘솔 폰트 크기 바꾸기
+}
